180c66fa016b4c7c5c42f44293a42e195df6d513
Justas Serstkovas <justas.serstkovas@dmg-media.co.uk>
2013-03-17 17:54:16 +0000

    tabbarviewcontoller added to iPhone storyboard

--- poconept_v.1/MasterViewController.m
+++ poconept_v.1/MasterViewController.m
@@ -123,7 +123,7 @@
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
         NSManagedObject *object = [[self fetchedResultsController] objectAtIndexPath:indexPath];
-        [[segue destinationViewController] setDetailItem:object];
+        //[[segue destinationViewController] setDetailItem:object];
     }
 }
 
